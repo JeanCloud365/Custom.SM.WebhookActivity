ab4c0720f7b0e96f1527fc7c232bcd6db29f8725
JanVanMeirvenne <jan@jvm-net.com>
2016-12-01 23:43:50 +0100

    added example runbooks

---
+++ RunbookExamples/New-User.ps1
@@ -0,0 +1,37 @@
+﻿ param (
+        [object]$WebhookData
+    )
+    $VerbosePreference = "Continue"
+    # If runbook was called from Webhook, WebhookData will not be null.
+    if ($WebhookData -ne $null) {
+
+        # Collect properties of WebhookData
+        $WebhookName     =     $WebhookData.WebhookName
+        $WebhookHeaders =     $WebhookData.RequestHeader
+        $WebhookBody     =     $WebhookData.RequestBody
+    }
+    try{
+        $ErrorActionPreference = "stop"
+		# Convert the JSON data posted by the SCSM Webhook Activity to a PS object
+        $userInfo = ConvertFrom-Json -InputObject $WebhookBody
+        Write-Verbose $userInfo
+    
+		# Get the automation credential from Azure Automation and use it to create the user in Azure AD
+        $cred = Get-AutomationPSCredential -Name "AutomationAccount"
+        Connect-MSOLService -Credential $cred
+        $Principal = "$($userInfo.First).$($userInfo.Last)@jvmscugdemo.onmicrosoft.com"
+        
+       
+        $User = New-MsolUser -UserPrincipalName $Principal -DisplayName $Principal -UsageLocation $userInfo.Country -MobilePhone $userInfo.Mobile
+        if($userInfo.License -eq "Yes"){
+            Set-MsolUserLicense -UserPrincipalName $Principal -AddLicenses "jvmscugdemo:INTUNE_A","jvmscugdemo:AAD_PREMIUM_P2","jvmscugdemo:ENTERPRISEPREMIUM"
+        }
+		# Call the SCSM update script inline
+        .\Set-SCSMRequest.ps1 -ActivityId $userInfo.ActivityId -Status Completed -Comment "User $($userInfo.First) created with parameters login '$Principal' / password '$($User.Password)'"
+    }
+    catch {
+	# If an issue happens, update the SCSM ticket accordingly as well
+        write-verbose "Error: $($_)"
+        .\Set-SCSMRequest.ps1 -ActivityId $userInfo.ActivityId -Status Failed -Comment "$($_)"
+
+    }
---
+++ RunbookExamples/Set-SCSMRequest.ps1
@@ -0,0 +1,44 @@
+﻿param(
+    $ActivityId,
+    $Status,
+    $Comment
+)
+# Import the smlets for easier SCSM management
+import-module smlets
+# Get the necessary info from the Azure Automation Assets
+$cred = Get-AutomationPSCredential -Name "AutomationAccount"
+$srv = Get-AutomationVariable -Name "SCSMServer"
+# Setup default parameters so that all SCSM commands are correctly send to the SCSM management server
+$PSDefaultParameterValues = @{
+    "*-SCSM*:Credential"=$cred;
+    "*-SCSM*:ComputerName"=$srv
+}
+
+# Using the SCSM Webhook Activities ID, fetch the associated service request
+$objActivity = Get-SCSMObject -Id $ActivityId
+$clsRequest = Get-SCSMClass -Name System.WorkItem.ServiceRequest
+$relRequestContainsActivity = Get-SCSMRelationshipClass -Name System.WorkItemContainsActivity
+
+$objRequest = (Get-SCSMRelationshipObject -ByTarget $objActivity|where{$_.SourceObject.ClassName -eq 'System.WorkItem.ServiceRequest'}).SourceObject
+# If a comment was passed to the runbook, place it in the SR through a projection
+if($Comment){
+    $NewGUID = ([Guid]::NewGuid()).ToString()
+    $Projection = @{__CLASS = "System.WorkItem.ServiceRequest";
+                    __SEED = $objRequest;
+                    AnalystCommentLog = @{__CLASS = "System.WorkItem.TroubleTicket.AnalystCommentLog";
+                                            __OBJECT = @{Id = $NewGUID;
+                                                        DisplayName = $NewGUID;
+                                                        Comment = $Comment;
+                                                        EnteredBy  = "SYSTEM";
+                                                        EnteredDate = (Get-Date).ToUniversalTime();
+                                                        IsPrivate = $false
+                                                        }
+                                            }
+                    }
+
+    New-SCSMObjectProjection -Type "System.WorkItem.ServiceRequestProjection" -Projection $Projection
+}
+# If a status change was passed to the runbook, update the SR status accordingly
+if($Status){
+    Set-SCSMObject -SMObject $objActivity -Property Status -Value $Status
+}
